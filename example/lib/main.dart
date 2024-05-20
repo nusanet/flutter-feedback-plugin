f1d4e7c3cffd78d9006aa8d9c0fd57161912d5bf
CoderJava <kolonel.yudisetiawan@gmail.com>
2024-05-20 15:05:09 +0700

    Update example mengenai penggunaan property `headerWidgets`

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -75,6 +75,9 @@ class _HomePageState extends State<HomePage> {
                             colorTitleAppBar: Colors.white,
                             colorAppBar: Colors.blue,
                             locale: locale,
+                            headerWidgets: [
+                              Text('ini header widget'),
+                            ],
                           );
                         },
                       ),
